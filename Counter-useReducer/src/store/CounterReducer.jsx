ee868e0e8c2cb96f2480401f4b9538cc45b8549d
Oweiskhaliq <101550838+Oweiskhaliq@users.noreply.github.com>
2025-01-23 13:17:39 +0500

    Update CounterReducer.jsx
    There is no need for a break statement when a return is used in switch.

--- Counter-useReducer/src/store/CounterReducer.jsx
+++ Counter-useReducer/src/store/CounterReducer.jsx
@@ -2,11 +2,11 @@ const CounterReducer = (state, action) => {
   switch (action.type) {
     case "INCREMENT":
       return state + 1;
-      break;
+      
 
     case "DECREMENT":
       return state - 1;
-      break;
+     
     case "RESET":
       return (state = 0);
     default:
